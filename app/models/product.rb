1e53744288c9459eae4a45c3bb0fb88547907aaf
Carlos Cantón <carloscant@gmail.com>
2016-03-12 18:14:18 +0100

    Validaciones en producto

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
   attr_accessible :descripcion, :imagen_url, :precio, :titulo
+
+  validates :titulo, :descripcion, :imagen_url, presence: true
+  validates :precio, numericality: {greater_than_or_equal_to: 0.01}
+  validates :titulo, uniqueness: true
+  validates :imagen_url, allow_blank: true, format: {
+  	with: %r{\.(gif|jpg|png)$}i,
+  	message: 'tiene que ser JPG, GIF o PNG.'
+  }
 end
