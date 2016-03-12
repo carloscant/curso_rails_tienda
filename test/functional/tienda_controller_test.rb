fa007a10700fa4ffae3ce3f8b46950dddfee1a1a
Carlos Cantón <carloscant@gmail.com>
2016-03-12 19:28:07 +0100

    Tests funcionales

--- test/functional/tienda_controller_test.rb
+++ test/functional/tienda_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class TiendaControllerTest < ActionController::TestCase
+
   test "should get index" do
     get :index
     assert_response :success
+    assert_select "h1", "Mi tienda"
+    assert_select ".producto", 3
+    assert_select ".precio", /[\d]+\.\d\d/
   end
 
 end
