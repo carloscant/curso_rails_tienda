f42b87393de1b46675f49571d626f0b4e81adec1
Carlos Cantón <carloscant@gmail.com>
2016-03-13 14:00:53 +0100

    Error en application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
 
 	private
 
-	def current_cart
+	def carro_actual
 
 		begin
-			Cart.find(session[:cart_id])
+			Carro.find(session[:carro_id])
 		rescue ActiveRecord::RecordNotFound
-			cart = Cart.create
-			session[:cart_id] = cart.id
-			cart
+			carro = Carro.create
+			session[:carro_id] = carro.id
+			carro
 		end
 		
 	end
